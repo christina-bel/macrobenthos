28c4fd48ccc9cfee2419df4e8b35d94c49ba0ecc
Vladimir Sudakov <vsudakov@bk.ru>
2021-01-05 00:28:48 +0300

    Update settings.py

--- macrobenthos/settings.py
+++ macrobenthos/settings.py
@@ -127,3 +127,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = "/static/"
+
+django_heroku.settings(locals())
