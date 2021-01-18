5f1ea23d37ded35e8a49d7504936a027731550fc
christina-bel <61422091+christina-bel@users.noreply.github.com>
2021-01-18 17:52:51 +0300

    Add files via upload

--- macrobenthos/urls.py
+++ macrobenthos/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('station/', include('station.urls')),
+    path('ship/', include('station.urls')),
     path('', include('station.urls'))
 ]
